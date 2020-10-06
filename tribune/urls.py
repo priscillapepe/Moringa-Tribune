5f3909177b5fafe790ecc82d73304f0a03ba4af6
priscillapepe <priscillaungai99@gmail.com>
2020-10-06 10:47:22 +0300

    added content to the files

--- tribune/urls.py
+++ tribune/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url,include
+from django.contrib import admin
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+      url(r'^admin/', admin.site.urls),
+    url(r'^news/',include('news.urls'))
 ]
